fd70be22e28a1895b252e2466e8b6edc0cf8d41a
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-02-06 14:44:35 -0300

    Criado estrutura de questionamentos javascript

---
+++ script.js
@@ -0,0 +1,92 @@
+const perguntas = [
+    {
+        pergunta: "Qual palavra-chave é usada para declarar uma variável em JavaScript?",
+        respostas: [
+            "let",
+            "var",
+            "const",
+        ],
+        correta: 0 // Resposta correta é "let"
+    },
+    {
+        pergunta: "Qual função é usada para imprimir algo no console do navegador?",
+        respostas: [
+            "print()",
+            "log()",
+            "console.log()",
+        ],
+        correta: 2 // Resposta correta é "console.log()"
+    },
+    {
+        pergunta: "Como você escreve um comentário de uma única linha em JavaScript?",
+        respostas: [
+            "// Este é um comentário",
+            "/* Este é um comentário */",
+            "' Este é um comentário",
+        ],
+        correta: 0 // Resposta correta é "// Este é um comentário"
+    },
+    {
+        pergunta: "Qual é a forma correta de comparar se duas variáveis são iguais em valor e tipo?",
+        respostas: [
+            "==",
+            "===",
+            "!=",
+        ],
+        correta: 1 // Resposta correta é "==="
+    },
+    {
+        pergunta: "Qual método é usado para remover o último elemento de um array em JavaScript?",
+        respostas: [
+            "pop()",
+            "shift()",
+            "splice()",
+        ],
+        correta: 0 // Resposta correta é "pop()"
+    },
+    {
+        pergunta: "Qual dos seguintes não é um tipo de dado em JavaScript?",
+        respostas: [
+            "string",
+            "number",
+            "integer",
+        ],
+        correta: 2 // Resposta correta é "integer"
+    },
+    {
+        pergunta: "Qual operador é usado para atribuição de valor em JavaScript?",
+        respostas: [
+            "=",
+            "==",
+            ":=",
+        ],
+        correta: 0 // Resposta correta é "="
+    },
+    {
+        pergunta: "O que o método 'charAt()' retorna se a posição especificada estiver fora do alcance de uma string?",
+        respostas: [
+            "null",
+            "undefined",
+            "Um erro",
+        ],
+        correta: 1 // Resposta correta é "undefined"
+    },
+    {
+        pergunta: "Qual dos seguintes métodos é usado para adicionar novos elementos ao final de um array?",
+        respostas: [
+            "push()",
+            "append()",
+            "addToEnd()",
+        ],
+        correta: 0 // Resposta correta é "push()"
+    },
+    {
+        pergunta: "Qual é a forma correta de declarar uma função em JavaScript?",
+        respostas: [
+            "função myFunction() {}",
+            "function = myFunction() {}",
+            "function myFunction() {}",
+        ],
+        correta: 2 // Resposta correta é "function myFunction() {}"
+    },
+];
